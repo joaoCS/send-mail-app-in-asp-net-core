b86fce07959a68a89d32f0361ce44aaf7d560699
JoaoCS <joaoantoniogba025@gmail.com>
2020-05-22 18:25:46 -0300

    Initial Commit

---
+++ sendEmailApp/Controllers/SendMailController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using sendEmailApp.Models;
+using System.Net.Mail;
+
+namespace sendEmailApp.Controllers
+{
+    public class SendMailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Email em)
+        {
+            string to = em.To;
+            string subject = em.Subject;
+            string body = em.Body;
+
+            MailMessage mm = new MailMessage();
+            mm.To.Add(to);
+            mm.Subject = subject;
+            mm.Body = body;
+            mm.From = new MailAddress("noreply1.2.3replyno@gmail.com");
+            mm.IsBodyHtml = false;
+
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+            smtp.Port = 587;
+            smtp.UseDefaultCredentials = true;
+            smtp.EnableSsl = true;
+
+            smtp.Credentials = new System.Net.NetworkCredential("noreply1.2.3replyno@gmail.com", "jo!@#123Abc");
+            smtp.Send(mm);
+
+            ViewBag.Message = "The mail has been sent to " + mm.To + " Sucessfully!";
+
+            return View();
+        }
+
+
+    }
+}
